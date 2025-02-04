5c64578fb02d7f2edf47aa88337ab54744804c14
= <79530088804@yandex.ru>
2025-02-04 12:31:10 +0500

    Generating PDF invoices dynamically

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,10 @@
+import weasyprint
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import Order, OrderItem
@@ -52,3 +57,16 @@ def admin_order_detail(request, order_id):
         'admin/orders/order/detail.html',
         {'order': order}
     )
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order_id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))],
+    )
+    return response
