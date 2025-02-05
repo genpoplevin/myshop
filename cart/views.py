3a71738f56ade5857a32896f7609ae47e3734d47
= <79530088804@yandex.ru>
2025-02-05 11:22:02 +0500

    Applying a coupon to the shopping cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
+from coupons.forms import CouponApplyForm
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
@@ -32,6 +33,15 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(
-            initial={'quantity': item['quantity'], 'override': True}
+            initial={'quantity': item['quantity'],
+                     'override': True}
         )
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(
+        request,
+        'cart/detail.html',
+        {
+            'cart': cart,
+            'coupon_apply_form': coupon_apply_form
+        }
+    )
--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,8 @@
+from decimal import Decimal
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from coupons.models import Coupon
 
 
 class Order(models.Model):
@@ -13,6 +16,17 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(
+        Coupon,
+        related_name='orders',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    discount = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
 
     class Meta:
         ordering = ['-created']
